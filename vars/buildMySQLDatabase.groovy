acebf6cf0a4c0152a074df6110efd1728548ea90
jciskey <jciskey@gmail.com>
2017-03-04 18:09:36 -0600

    Create buildMySQLDatabase.groovy
    Add the buildMySQLDatabase step implementation

---
+++ vars/buildMySQLDatabase.groovy
@@ -0,0 +1,37 @@
+#!/usr/bin/env groovy
+
+/*
+    Accepted Parameters:
+        mysqlPath: The system path to the MySQL binary. Default: /usr/bin/mysql
+        mysqlPort: The port to use to connect to MySQL. Default: 3306
+        dbName:    The name of the database to create. Default: "testdb_${env.BUILD_NUMBER}"
+        dbUser:    The name of the database user to create the database with.
+        dbPass:    The password of the database user.
+    
+    Returns the name of the created database.
+*/
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    // Set any unprovided configuration values to defaults
+    config.dbName = config.dbName ?: "testdb_${env.BUILD_NUMBER}"
+    config.mysqlPath = config.mysqlPath ?: '/usr/bin/mysql'
+    config.mysqlPort = config.mysqlPort ?: 3306
+    
+    // Check that required, non-defaultable values are present
+    if (config.dbUser == null) {
+        error "Missing required parameter 'dbUser'"
+    }
+    if (config.dbPass == null) {
+        error "Missing required parameter 'dbPass'"
+    }
+    
+    // Run shell commands to create the database here
+    
+    // Return the name of the created database
+    return config.dbName
+}

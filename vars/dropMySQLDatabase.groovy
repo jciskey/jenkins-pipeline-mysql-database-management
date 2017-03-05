1fe9adc1bb0f12c0876f42141cbba684b512539c
jciskey <jciskey@gmail.com>
2017-03-05 01:44:09 -0600

    Update dropMySQLDatabase.groovy
    Update step to use the configuration helper step

--- vars/dropMySQLDatabase.groovy
+++ vars/dropMySQLDatabase.groovy
@@ -9,24 +9,7 @@
         dbPass:    The password of the database user.
 */
 def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    
-    // Set any unprovided configuration values to defaults
-    config.dbName = config.dbName ?: "testdb_${env.BUILD_NUMBER}"
-    config.mysqlPath = config.mysqlPath ?: '/usr/bin/mysql'
-    config.mysqlPort = config.mysqlPort ?: 3306
-    
-    // Check that required, non-defaultable values are present
-    if (config.dbUser == null) {
-        error "Missing required parameter 'dbUser'"
-    }
-    if (config.dbPass == null) {
-        error "Missing required parameter 'dbPass'"
-    }
+    def config = evaluateMySQLDatabaseConfiguration(body)
     
     // Run shell commands to drop the database here
     def DROP_SQL = "DROP DATABASE IF EXISTS ${config.dbName};"

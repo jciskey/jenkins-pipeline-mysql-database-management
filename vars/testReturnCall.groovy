a02ee4a36a01a66cb7c580f6ffe0cd601ae02313
jciskey <jciskey@gmail.com>
2017-03-10 11:23:58 -0600

    Update testReturnCall.groovy

--- vars/testReturnCall.groovy
+++ vars/testReturnCall.groovy
@@ -10,7 +10,12 @@ def call(String param1 = 'test with single param default') {
     return param1
 }
 */
-
+/*
 def call(def param1 = null) {
     return param1?: 'null parameter passed'
+}
+*/
+
+def call(String dbUser = 'db_user', String dbPass = 'db_pass', def dbName = null, String mysqlPath = '/usr/bin/mysql', Integer mysqlPort = 3306) {
+    return 'test multiple parameters defined'
 }

52f9477620a59bf279523561f212795a960e1ab3
jciskey <jciskey@gmail.com>
2017-03-10 11:28:36 -0600

    Update testReturnCall.groovy

--- vars/testReturnCall.groovy
+++ vars/testReturnCall.groovy
@@ -15,7 +15,17 @@ def call(def param1 = null) {
     return param1?: 'null parameter passed'
 }
 */
-
+/*
 def call(String dbUser = 'db_user', String dbPass = 'db_pass', def dbName = null, String mysqlPath = '/usr/bin/mysql', Integer mysqlPort = 3306) {
     return 'test multiple parameters defined'
+}
+*/
+
+def call(String dbUser = 'db_user', String dbPass = 'db_pass', def dbName = null, String mysqlPath = '/usr/bin/mysql', Integer mysqlPort = 3306) {
+    
+    def retval = [:]
+    retval.username = dbUser
+    retval.password = dbPass
+    
+    return retval
 }

7c468f8d0d37275b54201709985daa713ae89e3e
jciskey <jciskey@gmail.com>
2017-03-10 15:34:21 -0600

    Update testReturnCall.groovy

--- vars/testReturnCall.groovy
+++ vars/testReturnCall.groovy
@@ -33,7 +33,7 @@ def call(Closure body) {
 
 def call(Closure body) {
     def config = [:]
-    //body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
     

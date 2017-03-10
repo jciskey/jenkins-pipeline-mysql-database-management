48cfe2483ee123f8a9172be9095a438bffeb07db
jciskey <jciskey@gmail.com>
2017-03-10 11:18:05 -0600

    Update testReturnCall.groovy

--- vars/testReturnCall.groovy
+++ vars/testReturnCall.groovy
@@ -5,7 +5,12 @@ def call() {
     return 'test no params'
 }
 */
-
+/*
 def call(String param1 = 'test with single param default') {
     return param1
+}
+*/
+
+def call(def param1 = null) {
+    return param1?: 'null parameter passed'
 }

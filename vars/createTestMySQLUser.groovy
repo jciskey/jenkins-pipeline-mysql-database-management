b0c194080e2920021b06478f380205be4dde07ff
Brady Owens <info@fastglass.net>
2018-12-30 13:31:53 -0500

    Usernames cannot be more than 32 char

--- vars/createTestMySQLUser.groovy
+++ vars/createTestMySQLUser.groovy
@@ -17,7 +17,8 @@ def call(Closure body) {
     def config = evaluateMySQLDatabaseConfiguration(body)
     
     // Define the test user parameters here
-    def test_user = "testdb_user_${config.uuid}"
+    String test_username = "testdb_user_${config.uuid}"
+    def test_user = test_username.take(32)
     def test_user_pwd = "testdb_password_${config.uuid}"
     
     // Run shell commands to create the user here

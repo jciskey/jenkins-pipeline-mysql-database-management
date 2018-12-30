d90d9e592a11cf0e333321ec5ca4ef0fbca6a823
Brady Owens <info@fastglass.net>
2018-12-30 15:24:09 -0500

    Put the UUID creation early in the process. Use a variable to blow away env.

--- vars/buildTestMySQLDatabase.groovy
+++ vars/buildTestMySQLDatabase.groovy
@@ -7,6 +7,7 @@
         dbName:    The name of the database to create. Default: "testdb_${env.BUILD_NUMBER}"
         dbUser:    The name of the database user to create the database with.
         dbPass:    The password of the database user.
+        newDB:      Boolean value to setup a new DB. Default: true.
     
     Returns the name of the created database as well as test user credentials in a Map.
         Map Keys:
@@ -16,7 +17,14 @@
 */
 String call(body) {
     def config = evaluateMySQLDatabaseConfiguration(body)
-    
+    // Create a unique id to use for database names and user accounts.
+    String uuid = UUID.randomUUID()
+    config.uuid  = uuid.replaceAll('-','_') + "_" + env.BUILD_NUMBER
+    // Store UUID into a Jenkins environment variable.
+    if (env.MYSQL_UUID == null || env.MYSQL_UUID == '' || config.newDB == true) {
+        env.MYSQL_UUID = config.uuid
+    }
+
     // Create the test database
     def createdDatabaseName = createMySQLDatabase {
         mysqlPath = config.mysqlPath

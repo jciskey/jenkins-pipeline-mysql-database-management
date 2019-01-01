a9e83043112ea0de6253d6633dfdb3664d97b8fc
Brady Owens <info@fastglass.net>
2019-01-01 12:50:52 -0500

    Debugging

--- vars/dropTestMySQLUser.groovy
+++ vars/dropTestMySQLUser.groovy
@@ -26,7 +26,10 @@ def call(String dbUserName, String dbPassword, String dbSchemaName, String dbDro
     }
 
     // Run shell commands to drop the user here
-    def DROP_SQL = "DROP USER '${test_user}';DROP USER '${test_user}'@'localhost';"
+    def DROP_SQL = "DROP USER '${test_user}'; DROP USER '${test_user}'@'localhost';"
+    echo "=============================== DEBUG ======================================"
+    echo DROP_SQL
+    echo "=============================== /DEBUG ======================================"
     def SHELL_CMD1 = "\"${dropconfig.mysqlPath}\" -u \"${dropconfig.dbUser}\" --password=\"${dropconfig.dbPass}\" <<-EOF\n${DROP_SQL}\nEOF"
     sh "${SHELL_CMD1}"
     def REVOKE_SQL = "REVOKE ALL PRIVILEGES, GRANT OPTION FROM '${test_user}';"
